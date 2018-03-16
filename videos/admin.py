15e589eb2ac4fd1f33855311789b9275fb253ae6
vanderkilu <vanderkiliku@gmail.com>
2018-03-16 00:42:50 -0400

    register models

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from videos.models import Video,Playlist,Tags,Category
 
 # Register your models here.
+admin.site.register(Playlist)
+admin.site.register(Tags)
+admin.site.register(Category)
+admin.site.register(Video)
