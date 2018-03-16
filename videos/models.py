9102752a458dd0bab32403ddd0d387b69f608a73
vanderkilu <vanderkiliku@gmail.com>
2018-03-16 00:15:36 -0400

    create models

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=5000)
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=5000)
+
+
+class Video(models.Model):
+    upload = models.FileField()
+    title = models.CharField(max_length=50)
+    views = models.IntegerField()
+    description = models.CharField(max_length=5000)
+    date_uploaded = models.DateField(auto_now=True)
+    likes = models.IntegerField()
+    tags = models.ManyToManyField(Tags)
+    category = models.ManyToManyField(Category)
+    playlist = models.ForeignKey(Playlist, related_name="videos")
+
+
+
